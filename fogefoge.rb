adbe7a63646ad93c98de81b903769580341547c1
Marcio Santos <martivs@gmail.com>
2020-03-09 12:41:58 -0300

    alterações na movimentação dos fantasmas

--- fogefoge.rb
+++ fogefoge.rb
@@ -8,7 +8,16 @@ def encontra_jogador(mapa)
             return [linha, coluna_do_heroi]
         end
     end
-    # não achei!
+    nil
+end
+
+def jogador_perdeu?(mapa)
+    perdeu = !encontra_jogador(mapa)
+end
+
+def game_over
+    puts "\n\n\n\n\n\n"
+    puts "Game Over"
 end
 
 def joga(nome)
@@ -20,14 +29,18 @@ def joga(nome)
         heroi = encontra_jogador mapa
         nova_posicao = calculaNovaPosicao heroi, direcao
 				
-				if !posicao_valida? mapa, nova_posicao
+        if !posicao_valida? mapa, nova_posicao
             next
         end
 			
 
         mapa[heroi[0]][heroi[1]] = " "
         mapa[nova_posicao[0]][nova_posicao[1]] = "H"
-				move_fantasmas mapa
+		mapa = move_fantasmas mapa
+        if jogador_perdeu?(mapa)
+            game_over
+            break
+        end
     end
 end
 
@@ -50,44 +63,69 @@ def calculaNovaPosicao heroi,direcao
 	heroi
 end
 
-def move_fantasma(mapa, linha, coluna)
-    posicao = [linha, coluna + 1]
-    if posicao_valida? mapa, posicao
-        mapa[linha][coluna] = " "
-        mapa[posicao[0]][posicao[1]] = "F"
+def soma(vetor1, vetor2)
+    [vetor1[0] + vetor2[0], vetor1[1] + vetor2[1]]
+end
+
+def posicoes_validas_a_partir_de(mapa, novo_mapa, posicao)
+    posicoes = []
+    movimentos = [[-1, 0], [0, +1], [+1, 0], [0, -1]]
+    #movimentos = [[-2, -1], [-2, +1], [+2, -1], [+2, +1],[-1, -2], [-1, +2], [+1, -2], [+1, +2]]
+    movimentos.each do |movimento|
+        nova_posicao = soma posicao,movimento
+        if posicao_valida?(mapa, nova_posicao) && posicao_valida?(novo_mapa, nova_posicao)
+            posicoes << nova_posicao
+        end
     end
+    posicoes
+end
+
+def move_fantasma(mapa, novo_mapa, linha, coluna)
+    posicoes = posicoes_validas_a_partir_de mapa,novo_mapa, [linha, coluna]
+    if posicoes.empty?
+        return
+    end
+    aleatoria = rand posicoes.size
+    posicao = posicoes[aleatoria]
+
+    mapa[linha][coluna] = " "
+    novo_mapa[posicao[0]][posicao[1]] = "F"
+    
+end
+
+def copia_mapa(mapa)
+  novo_mapa = mapa.join("\n").tr("F", " ").split("\n")
 end
 
 def move_fantasmas(mapa)
     caracter_do_fantasma = "F"
+    novo_mapa = copia_mapa mapa
     mapa.each_with_index do |linha_atual, linha|
         linha_atual.chars.each_with_index do |caractere_atual, coluna|
             eh_fantasma = caractere_atual == caracter_do_fantasma
             if eh_fantasma
-                move_fantasma mapa, linha, coluna
+                move_fantasma mapa,novo_mapa, linha, coluna
             end
         end
     end
+    novo_mapa
 end
 
 def posicao_valida? mapa, posicao
-	if posicao[0] < 0
+	linhas = mapa.size
+    colunas = mapa[0].size
+
+    estourou_linha = posicao[0] < 0 || posicao[0] >= linhas
+    estourou_coluna = posicao[1] < 0 || posicao[1] >= colunas
+
+    if estourou_linha || estourou_coluna
         return false
     end
-    if posicao[1] < 0
+
+    if mapa[posicao[0]][posicao[1]] == "X"
         return false
     end
-    if posicao[0] >= mapa.size
-        return false
-    end
-    if posicao[1] >= mapa[0].size
-        return false
-    end
-	
-    valor_local = mapa[posicao[0]][posicao[1]]
-    if valor_local == "X" || valor_local == "F"
-        return false
-    end
+
     true
 end
 
